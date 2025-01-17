9af0511c47d95c1e182c58bebf7e4979700b30cc
mhmd <mohammadoshkooh@gmail.com>
2025-01-18 02:21:14 +0330

    add postgres

--- core/settings.py
+++ core/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config('POSTGRES_NAME'),
+        "USER": config('POSTGRES_USER'),
+        "PASSWORD": config('POSTGRES_PASSWORD'),
+        "HOST": 'postgres',
+        "PORT": "5432"}
 }
 
 # Password validation
